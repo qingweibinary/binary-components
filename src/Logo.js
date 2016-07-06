aad1667d5e65081ab3e5951c8f637cd162cea61c
Qing Wei <qingwei@binary.com>
2016-07-07 01:04:24 +0800

    Use relative path for img (#3)
    * Use relative path for img
    
    * Bump version, published
    
    * FIx img link

--- src/Logo.js
+++ src/Logo.js
@@ -7,7 +7,7 @@ export default class Logo extends Component {
 	render() {
 		return (
 			<div id="logo">
-				<img src="/img/binary-symbol-logo.svg" role="presentation" />
+				<img src="img/binary-symbol-logo.svg" role="presentation" />
 			</div>
 		);
 	}
