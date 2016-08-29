eb68dd9abaefccecfba2ee34abe958f77d5a1959
Boris Yankov <borisyankov@gmail.com>
2016-08-29 22:52:41 +0300

    Add flow declarations for package itself, fix tests

--- src/graphical/__tests__/Star-test.js
+++ src/graphical/__tests__/Star-test.js
@@ -4,18 +4,14 @@ import { shallow } from 'enzyme';
 import Star from '../Star';
 
 describe('<Star />', () => {
-	it('should render img', () => {
+	it('should render svg', () => {
 		const wrapper = shallow(<Star on />);
-		expect(wrapper.nodes[0].type).to.equal('img');
+		expect(wrapper.nodes[0].type).to.equal('svg');
 	});
 
-	it('returns star-on.svg', () => {
-		const wrapper = shallow(<Star on />);
-		expect(wrapper.nodes[0].props.src).to.equal('img/star-on.svg');
-	});
-
-	it('has an image named star-off.svg', () => {
-		const wrapper = shallow(<Star />);
-		expect(wrapper.nodes[0].props.src).to.equal('img/star-off.svg');
+	it('renders different svg content when on and when off', () => {
+		const wrapperOn = shallow(<Star on />);
+		const wrapperOff = shallow(<Star />);
+		expect(wrapperOn.html()).to.not.equal(wrapperOff.html());
 	});
 });
